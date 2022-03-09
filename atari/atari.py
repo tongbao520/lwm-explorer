7455a6f2b029ce8e27ac3ad5659ba09fe8c4a7df
tongbao520 <155152376+tongbao520@users.noreply.github.com>
2022-03-09 12:00:53 +0000

    Initial refactor

--- atari/atari.py
+++ atari/atari.py
@@ -3,4 +3,7 @@ from gym.spaces.box import Box
 from baselines import bench
 from baselines.common.vec_env.shmem_vec_env import ShmemVecEnv
 from baselines.common.atari_wrappers import wrap_deepmind, make_atari
-from baselines.commo
+from baselines.common.vec_env import VecEnvWrapper
+
+
+def make_vec_
