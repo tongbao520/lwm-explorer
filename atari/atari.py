826a60f0cd3cbff1b73f183a9b6f8d7fdeb71b4a
tongbao520 <155152376+tongbao520@users.noreply.github.com>
2022-03-12 08:33:54 +0000

    Envi Template:

--- atari/atari.py
+++ atari/atari.py
@@ -6,4 +6,7 @@ from baselines.common.atari_wrappers import wrap_deepmind, make_atari
 from baselines.common.vec_env import VecEnvWrapper
 
 
-def make_vec_envs(name, num, seed=0, max_ep_len=1000
+def make_vec_envs(name, num, seed=0, max_ep_len=100000):
+    def make_env(rank):
+        def _thunk():
+    
