553dc447d33c3a16d34a2d43963db480382e47e7
tongbao520 <155152376+tongbao520@users.noreply.github.com>
2022-04-06 12:32:56 +0000

    chore: remove unused code

--- atari/common/optim.py
+++ atari/common/optim.py
@@ -2,4 +2,8 @@ from dataclasses import dataclass
 from typing import List
 import torch
 from torch.nn.utils import clip_grad_norm_
-from torch.o
+from torch.optim import Adam
+
+
+@dataclass
+class ParamOptim:
