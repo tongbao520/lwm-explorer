caa46cbbcb06c47659064a1864de9868e766ef6d
tongbao520 <155152376+tongbao520@users.noreply.github.com>
2022-04-26 16:24:20 +0000

    Change language

--- atari/common/timer.py
+++ atari/common/timer.py
@@ -17,4 +17,5 @@ def timer_log(num_iter=1000):
         if mark is None and len(list(mean_t.values())[0]) >= num_iter:
             log = {"time/" + k: np.mean(v) * 1000 for k, v in mean_t.items()}
             mean_t = defaultdict(list)
-       
+        else:
+            log = {}
