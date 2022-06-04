07150601c31a35e69662167404e59bc52408f658
tongbao520 <155152376+tongbao520@users.noreply.github.com>
2022-06-04 09:42:01 +0000

    burn fix

--- atari/dqn/algo.py
+++ atari/dqn/algo.py
@@ -52,4 +52,6 @@ def get_td_error(batch, hx_start, model, model_t, cfg, need_stat=False):
     done = batch["done"][burnin + pf + 1 : -n_step + 1].float()
 
     q = qs[:-n_step].gather(2, action)
-    ns_action = qs[1:].argmax(2)[..., None].detach
+    ns_action = qs[1:].argmax(2)[..., None].detach()
+    next_q = qs_target.gather(2, ns_action)
+   
