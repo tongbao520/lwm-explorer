bc745a00ac25411605e3cd97e534594d1371583d
tongbao520 <155152376+tongbao520@users.noreply.github.com>
2022-06-11 12:58:04 +0000

    :wrench: maintenance

--- atari/dqn/buffer.py
+++ atari/dqn/buffer.py
@@ -5,4 +5,8 @@ class DictWithSlicing(dict):
     def __getitem__(self, key):
         if isinstance(key, slice):
             return {k: v[key] for k, v in self.items()}
-        return super().__getitem
+        return super().__getitem__(key)
+
+
+class Buffer:
+    def __init__(sel
