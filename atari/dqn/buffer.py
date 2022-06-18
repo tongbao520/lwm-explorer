9a2434b87014d6332509bab1a2f5279dfcf3f659
tongbao520 <155152376+tongbao520@users.noreply.github.com>
2022-06-18 17:32:28 +0000

    js build

--- atari/dqn/buffer.py
+++ atari/dqn/buffer.py
@@ -10,4 +10,5 @@ class DictWithSlicing(dict):
 
 class Buffer:
     def __init__(self, maxlen, num_env, obs_shape, device):
-        self.maxlen, self.num_env, self.device = maxl
+        self.maxlen, self.num_env, self.device = maxlen, num_env, device
+        self.
