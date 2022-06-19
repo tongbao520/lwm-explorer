6b08057d78b0a5eb33b4aacade7472d4bdf8f795
tongbao520 <155152376+tongbao520@users.noreply.github.com>
2022-06-19 11:07:50 +0000

    PopUp Menu

--- atari/dqn/buffer.py
+++ atari/dqn/buffer.py
@@ -11,4 +11,6 @@ class DictWithSlicing(dict):
 class Buffer:
     def __init__(self, maxlen, num_env, obs_shape, device):
         self.maxlen, self.num_env, self.device = maxlen, num_env, device
-        self.
+        self.reset()
+
+        def tensor(shape=(1,), dtype=tor
