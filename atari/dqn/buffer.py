7638cf0338cd6c26bf3315edc2f52f23b610703a
tongbao520 <155152376+tongbao520@users.noreply.github.com>
2022-07-21 20:02:02 +0000

    * minor fix

--- atari/dqn/buffer.py
+++ atari/dqn/buffer.py
@@ -51,4 +51,5 @@ class Buffer:
     def get_recent(self, steps, device="cuda"):
         if len(self) == 0:
             return None
-        idx = torch.tensor([self.cursor - 1] * 
+        idx = torch.tensor([self.cursor - 1] * self.num_env)
+        idx_env = torch.arange(se
