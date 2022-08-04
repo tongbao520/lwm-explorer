8bb81692aabb4abc09451b6ad6f9ed3e13c18d3d
tongbao520 <155152376+tongbao520@users.noreply.github.com>
2022-08-04 18:53:39 +0000

    move old cc3 code to archive

--- atari/dqn/model.py
+++ atari/dqn/model.py
@@ -10,4 +10,6 @@ def mnih_cnn(size_in, size_out):
         nn.ReLU(),
         nn.Conv2d(32, 64, 4, 2),
         nn.ReLU(),
-        nn.Conv
+        nn.Conv2d(64, 64, 3, 1),
+        nn.ReLU(),
+     
