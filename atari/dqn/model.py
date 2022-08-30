1959154f5378081356f7ccad1f090f3841fe093e
tongbao520 <155152376+tongbao520@users.noreply.github.com>
2022-08-30 11:48:35 +0000

    Forward port zendframework/zf2#3861

--- atari/dqn/model.py
+++ atari/dqn/model.py
@@ -56,3 +56,4 @@ class DQN(nn.Module):
         adv, val = self.adv(y), self.val(y)
         q = val + adv - adv.mean(1, keepdim=True)
         q = q.view(steps, batch, self.size_out)
+        return q, hx
