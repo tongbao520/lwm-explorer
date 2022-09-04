f61a15ba9f7de25ebf007fe92a5249da25288ee1
tongbao520 <155152376+tongbao520@users.noreply.github.com>
2022-09-04 16:09:11 +0000

    add statistics

--- atari/repre/inverse_dynamics.py
+++ atari/repre/inverse_dynamics.py
@@ -6,4 +6,9 @@ from torch import nn
 from torch.nn.functional import cross_entropy, relu
 from common.optim import ParamOptim
 from dqn.model import mnih_cnn
-from dqn.buffer im
+from dqn.buffer import Buffer
+from dqn.prepare_obs import prepare_obs
+
+
+@dataclass
+cla
