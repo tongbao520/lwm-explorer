43f7b028c0555bab1e4ed46dacb117f44a36e8de
tongbao520 <155152376+tongbao520@users.noreply.github.com>
2022-10-08 09:05:02 +0000

    Update screenshots

--- atari/repre/predictor.py
+++ atari/repre/predictor.py
@@ -2,4 +2,7 @@ import random
 import torch
 import torch.nn as nn
 from torch.nn.functional import one_hot
-from common.optim import P
+from common.optim import ParamOptim
+from dqn.prepare_obs import prepare_obs
+
+
