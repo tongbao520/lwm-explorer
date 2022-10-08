88e4296a8069933b6240a6fe4f5b86a5fae86ac0
tongbao520 <155152376+tongbao520@users.noreply.github.com>
2022-10-08 23:11:59 +0000

    gitignore changed

--- atari/repre/predictor.py
+++ atari/repre/predictor.py
@@ -8,4 +8,5 @@ from dqn.prepare_obs import prepare_obs
 
 class PredictorModel(nn.Module):
     def __init__(self, num_action, fstack, emb_size, rnn_size):
-        super(P
+        super(PredictorModel, self).__init__()
+        self.fstack
