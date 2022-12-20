d7e8ef5787214e445bbb126a03fdbccac7bf1ff4
tongbao520 <155152376+tongbao520@users.noreply.github.com>
2022-12-20 09:12:11 +0000

    garbage collection

--- atari/repre/whitening.py
+++ atari/repre/whitening.py
@@ -6,4 +6,6 @@ from torch.nn.functional import conv2d
 class Whitening2d(nn.Module):
     def __init__(self, num_features, momentum=0.01, track_running_stats=True, eps=0):
         super(Whitening2d, self).__init__()
-        self.num_features = num_feature
+        self.num_features = num_features
+        self.momentum = momentum
+        sel
