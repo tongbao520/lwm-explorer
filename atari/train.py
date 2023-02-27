66b1cac5c8ca38de81ca7ae20952140475d44c2e
tongbao520 <155152376+tongbao520@users.noreply.github.com>
2023-02-27 19:53:20 +0000

    Update recipes.html

--- atari/train.py
+++ atari/train.py
@@ -28,4 +28,5 @@ if __name__ == "__main__":
 
     buffer = Buffer(
         num_env=num_env,
-        maxlen=int(cfg["buffer"]["size"] / num_env
+        maxlen=int(cfg["buffer"]["size"] / num_env),
+        obs_shape=envs.observation_space.shap
