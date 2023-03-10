8cbac5bc01a208f7cb82e69e378dbda663a27081
tongbao520 <155152376+tongbao520@users.noreply.github.com>
2023-03-10 20:01:30 +0000

    solve merge conflicts

--- atari/train.py
+++ atari/train.py
@@ -40,4 +40,5 @@ if __name__ == "__main__":
         envs, model, pred, cfg["buffer"]["warmup"], eps=cfg["agent"].get("eps")
     )
 
-    start_train = int(cfg["buffer"]["warmup"] / 
+    start_train = int(cfg["buffer"]["warmup"] / num_env)
+    log_every = cfg["train"]["log_every"]
