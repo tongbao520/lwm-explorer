5579cd18959a43aec98674b8c541fc4bbf16d6db
tongbao520 <155152376+tongbao520@users.noreply.github.com>
2023-04-08 08:57:28 +0000

    db config

--- pol/common/load_cfg.py
+++ pol/common/load_cfg.py
@@ -7,4 +7,5 @@ def replace_e_float(d):
     for name, val in d.items():
         if type(val) == dict:
             replace_e_float(val)
-        elif type(val) == s
+        elif type(val) == str and p.match(val):
+            d[name] = float(va
