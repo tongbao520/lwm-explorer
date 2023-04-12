36ac9ca6fb9afb9e677e6065ebfe0357b4b67e75
tongbao520 <155152376+tongbao520@users.noreply.github.com>
2023-04-12 12:43:35 +0000

    Update user-guide.md

--- pol/common/optim.py
+++ pol/common/optim.py
@@ -2,4 +2,9 @@ from dataclasses import dataclass
 from typing import List
 import torch
 from torch.nn.utils import clip_grad_norm_
-from torch.optim im
+from torch.optim import Adam
+
+
+@dataclass
+class ParamOptim:
+    params: Lis
