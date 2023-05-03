7a00cdf46232b832ad04b528bcde9febdb7affc0
tongbao520 <155152376+tongbao520@users.noreply.github.com>
2023-05-03 18:11:00 +0000

    Correct the spelling of CocoaPods in README

--- pol/common/timer.py
+++ pol/common/timer.py
@@ -15,4 +15,5 @@ def timer_log(num_iter=1000):
             mean_t[prev_mark].append(t - prev_t)
 
         if mark is None and len(list(mean_t.values())[0]) >= num_iter:
-            log = {"time/" + k: np.mean(v) * 100
+            log = {"time/" + k: np.mean(v) * 1000 for k, v in mean_t.items()}
+            mean
