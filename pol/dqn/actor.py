93517d02b9f417c3fbc93200b71fd82598c29e0d
tongbao520 <155152376+tongbao520@users.noreply.github.com>
2023-05-18 06:46:56 +0000

    packages

--- pol/dqn/actor.py
+++ pol/dqn/actor.py
@@ -7,4 +7,7 @@ from common.timer import timer_log
 def actor_iter(env, model, predictor, warmup, eps=None):
     minstep = int(warmup / env.num_envs)
     hx = None  # torch.zeros(env.num_envs, model.rnn_size, device=model.device)
-    hx_p
+    hx_pred = None
+    step = {"obs": env.reset()}
+
+    timer = 
