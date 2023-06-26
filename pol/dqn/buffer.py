b04ae991ad1f9baf5f4004045c62a42fa3338a68
tongbao520 <155152376+tongbao520@users.noreply.github.com>
2023-06-26 20:33:46 +0000

    update gemspec

--- pol/dqn/buffer.py
+++ pol/dqn/buffer.py
@@ -3,4 +3,5 @@ import torch
 
 class DictWithSlicing(dict):
     def __getitem__(self, key):
-        if isinstance(key
+        if isinstance(key, slice):
+            return {k: v[key] for k, v in s
