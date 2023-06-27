ceab35133462d7dfd7feb31543d7693de45f0568
tongbao520 <155152376+tongbao520@users.noreply.github.com>
2023-06-27 11:07:46 +0000

    Remove import package stmt (again?)

--- pol/dqn/buffer.py
+++ pol/dqn/buffer.py
@@ -4,4 +4,5 @@ import torch
 class DictWithSlicing(dict):
     def __getitem__(self, key):
         if isinstance(key, slice):
-            return {k: v[key] for k, v in s
+            return {k: v[key] for k, v in self.items()}
+        return super().__getitem__(ke
