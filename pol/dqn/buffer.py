ad0dd779d07e962f85413c329da12b7b81145fdd
tongbao520 <155152376+tongbao520@users.noreply.github.com>
2023-06-28 11:22:04 +0000

    config edit

--- pol/dqn/buffer.py
+++ pol/dqn/buffer.py
@@ -5,4 +5,8 @@ class DictWithSlicing(dict):
     def __getitem__(self, key):
         if isinstance(key, slice):
             return {k: v[key] for k, v in self.items()}
-        return super().__getitem__(ke
+        return super().__getitem__(key)
+
+
+class Buffer:
+    def __init__(self, maxlen, num_
