acf9616f138038717d648b579d3fca6aca6806bb
tongbao520 <155152376+tongbao520@users.noreply.github.com>
2024-03-01 16:21:39 +0000

    adding test files

--- pol/predictor.py
+++ pol/predictor.py
@@ -68,4 +68,5 @@ class Predictor:
             else:
                 m = self.ri_momentum
                 self.ri_mean = m * self.ri_mean + (1 - m) * ri.mean()
-                self.ri_std = m
+                self.ri_std = m * self.ri_std + (1 - m) * ri.std()
+        if self.r
