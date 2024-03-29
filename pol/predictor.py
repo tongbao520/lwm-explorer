09985681bbea70e50eb604f1437a01ec66fb730d
tongbao520 <155152376+tongbao520@users.noreply.github.com>
2024-03-29 15:04:26 +0000

    Add new blog

--- pol/predictor.py
+++ pol/predictor.py
@@ -79,4 +79,5 @@ class Predictor:
         # this function is used only for pretrain, main training loop is in dqn learner
         batch_size = 64
         sample_steps = 100
-        if len(se
+        if len(self.buffer) < self.buffer.maxlen:
+            no_pr
