dd5e7b19c7fba1029f34d9936c1621f22732fa4b
tongbao520 <155152376+tongbao520@users.noreply.github.com>
2024-04-08 21:53:18 +0000

    Remove stuff

--- pol/predictor.py
+++ pol/predictor.py
@@ -83,4 +83,5 @@ class Predictor:
             no_prev = set(range(sample_steps))
         else:
             no_prev = set(
-                (self.buffer.cursor + i) % 
+                (self.buffer.cursor + i) % self.buffer.maxlen
+                for i in range(sample_
